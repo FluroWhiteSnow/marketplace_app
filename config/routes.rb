3bb0c6944ff3f74dcb0f06a36750e46fdc8a7d73
Ananda <anrodziewiczz@gmail.com>
2021-05-19 20:51:21 +1000

    Added styling for sign up and sign in

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,12 @@
 Rails.application.routes.draw do
   devise_for :users
-  
-  root "pages#home"
 
-  get 'pages/home'
-  get 'pages/restricted'
-  get 'pages/about'
-  get 'pages/buy'
-  get 'pages/sell'
+  root "pages#home"
+  
+  get '/restricted', to: 'pages#restricted', as: 'restricted'
+  get '/about',      to: "pages#about"
+  get '/buy',        to: "pages#buy"
+  get '/sell',       to: "pages#sell"
+
 
 end
