5a103eeba0530d454b5b61d2d8cf6fd5a4d26ca4
Ananda <anrodziewiczz@gmail.com>
2021-05-24 13:55:29 +1000

    added migrations to listing, title, price description

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_19_074041) do
+ActiveRecord::Schema.define(version: 2021_05_24_034959) do
+
+  create_table "listings", force: :cascade do |t|
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.string "title"
+    t.text "description"
+    t.integer "price"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
