b506b84aac321c60a43e19bdc556e10598f6a8ea
Ananda <anrodziewiczz@gmail.com>
2021-05-27 13:49:30 +1000

    switched to postgresql database

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,5 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :timeoutable
-
     
 end
