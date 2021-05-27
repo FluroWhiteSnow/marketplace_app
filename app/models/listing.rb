4b1e144f8d79a6eb84e7f337d52bea1a3c5d8675
Ananda <anrodziewiczz@gmail.com>
2021-05-27 12:47:10 +1000

    removed about page

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -2,5 +2,7 @@ class Listing < ApplicationRecord
     belongs_to :user
     has_many_attached :images  
     has_one :purchase, dependent: :destroy
+
     validates :title, :price, presence: true
+
 end
